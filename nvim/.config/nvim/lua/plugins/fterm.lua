b319112609e67acc3e7cbb2b090bd3eadba0cc86
3nricomarinelli <enrico.marinelli2910@gmail.com>
2026-02-17 14:34:45 +0100

    neovim floating terminal inherits env terminal

--- nvim/.config/nvim/lua/plugins/fterm.lua
+++ nvim/.config/nvim/lua/plugins/fterm.lua
@@ -1,5 +1,12 @@
 local fterm = require("FTerm")
 
+-- Configure FTerm to use the system's $TERM value
+fterm.setup({
+	env = {
+		TERM = vim.env.TERM or "xterm-256color"
+	}
+})
+
 _G.htop = fterm:new({
 	ft = 'fterm_htop',
 	cmd = "htop"
