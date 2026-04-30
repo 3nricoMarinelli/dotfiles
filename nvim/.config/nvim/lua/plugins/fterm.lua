38ed5837ee73a0f10b818e70229bf2c07b50365d
3nricomarinelli <enrico.marinelli2910@gmail.com>
2026-04-30 12:24:46 +0200

    neovim telescope handling hidden files

--- nvim/.config/nvim/lua/plugins/fterm.lua
+++ nvim/.config/nvim/lua/plugins/fterm.lua
@@ -2,12 +2,13 @@ local fterm = require("FTerm")
 
 -- Configure FTerm to use the system's $TERM value
 fterm.setup({
-	env = {
-		TERM = vim.env.TERM or "xterm-256color"
-	}
+  env = {
+    TERM = vim.env.TERM or "xterm-256color",
+    FLOATING_TERM = 1,
+  },
 })
 
 _G.htop = fterm:new({
-	ft = 'fterm_htop',
-	cmd = "htop"
+  ft = "fterm_htop",
+  cmd = "htop",
 })
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -10,9 +10,19 @@ require("telescope").setup({
   pickers = {
     find_files = {
       theme = "dropdown",
+      find_command = {
+        "fd",
+        "--type",
+        "f",
+        "--hidden",
+        "--follow",
+        "--exclude",
+        ".git",
+      },
     },
     live_grep = {
       theme = "dropdown",
+      additional_args = { "--hidden", "--follow", "--glob", "!.git" },
     },
     grep_string = {
       theme = "dropdown",
